51a11a2f5f10c7da515052f2069a38c613cb6063
Gustavo Menezes <143659080+Gustavomenezes11@users.noreply.github.com>
2024-01-30 18:28:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+//Função de Calcular//
+function calcular() {
+    const ano = document.getElementsByClassName("anoquenasceu")[0].value
+    const calculoum = 2024 - ano
+    const resultado = document.getElementsByClassName("resultadonatela")[0].innerHTML = calculoum
+}
+
+//Pagina no modo azul//
+function modoazul(){
+    document.body.style.backgroundColor= "DarkBlue"
+    const corazul = document.getElementById("campoprincipal").style.backgroundColor="DarkBlue"
+}
+
+//Pagina no modo azul//
+function modovermelho(){
+    document.body.style.backgroundColor= "DarkRed"
+    const corvermelho = document.getElementById("campoprincipal").style.backgroundColor = "DarkRed"
+}
